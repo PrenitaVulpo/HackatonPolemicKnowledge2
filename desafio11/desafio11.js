5ff34388f81f36b7da6ed29954f2f6ede579d1c5
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-08-24 00:03:48 -0300

    a

--- desafio11/desafio11.js
+++ desafio11/desafio11.js
@@ -8,11 +8,19 @@ Crie uma função que retorne as coordenadas ([linha, col]) do elemento que dife
 
 function ondeEstaWally(arr) {
     wally = '';
+    coord = [];
     for (i =0;i<arr.length;i++){
         for (c =0;c<arr[i].length){
             if (wally != arr[i,c]) wally = arr[i,c]
         }
     }
+    for (i =0;i<arr.length;i++){
+        for (c =0;c<arr[i].length){
+            if (wally == arr[i,c]) coord.push(i);
+            if (wally == arr[i,c]) coord.push(c);
+        }
+    }
+    return coord
 }
 
 
